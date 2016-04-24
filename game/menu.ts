1ecb24029bee50d0f8ece65f21914f0455b298bb
Ge Yan <ivy951214@gmail.com>
2016-04-24 01:05:29 -0400

    Menu view

--- game/menu.ts
+++ game/menu.ts
@@ -9,12 +9,20 @@ module BrickStep {
         button_normal;
         button_zen;
         button_setting;
+        button_empty;
+        text_normal;
 
         create() {
             //TODO: menu scene set up
-            this.button_normal = this.game.add.button(240, 0, 'white', this.startNormal, this);
+            this.button_normal = this.game.add.button(240, 0, 'white', this.startNormal, this, 2, 1, 0);
             this.button_zen = this.game.add.button(0, 160, 'white', this.startZen, this);
             this.button_setting = this.game.add.button(240, 320, 'white', this.startZen, this);
+            this.button_empty = this.game.add.button(0, 480,'white');
+
+            //setting texts
+            var style = {font: "32px Futura condensed", align: "center"};
+            this.text_normal= this.game.add.text(0, 0, "Normal", style);
+
         }
         
         private startZen() {
