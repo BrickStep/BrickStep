c502cfd697bbc13e5491e26e83eabd256156241f
Linda <locoda@foxmail.com>
2016-04-24 00:57:21 -0400

    - update the normal game that can run

--- game/main/normal.ts
+++ game/main/normal.ts
@@ -9,7 +9,6 @@ module BrickStep {
 
         create() {
             this.tiles = this.game.add.group();
-
             this.timer = this.game.time.events.loop(800, this.addRowOfTiles, this);
         }
 
@@ -20,12 +19,12 @@ module BrickStep {
             // Add to created group
             this.tiles.add(tile);
 
-            // Add velocity to the pipe to make it move
-            tile.body.velocity.y = -200;
 
-            // Automatically kill the pipe when it's no longer visible
-            tile.checkWorldBounds = true;
-            tile.outOfBoundsKill = true;
+            // Add velocity to the tile to make it move
+            this.game.physics.arcade.enable(tile);
+            tile.body.velocity.y = 200;
+
+            //tile.events.onEnterBounds.add(this.destoryLisenter, tile);
         }
 
 
@@ -36,26 +35,29 @@ module BrickStep {
             // Add to created group
             this.tiles.add(tile);
 
-            // Add velocity to the pipe to make it move
-            tile.body.velocity.y = -200;
+            // Add velocity to the tile to make it move
+            this.game.physics.arcade.enable(tile);
+            tile.body.velocity.y = 200;
 
+            tile.events.onEnterBounds.add(this.destoryLisenter, tile);
+        }
+
+        private destoryLisenter(tile) {
             // Automatically kill the pipe when it's no longer visible
             tile.checkWorldBounds = true;
             tile.outOfBoundsKill = true;
         }
 
         addRowOfTiles() {
-            // Randomly pick a number between 1 and 5
-            // This will be the hole position
-            var black = Math.floor(Math.random() * 4) + 1;
+            // random roll a number from 0 to 3
+            var black = Math.floor(Math.random() * 4);
 
-            // Add the 6 pipes
-            // With one big hole at position 'hole' and 'hole + 1'
+            // the rolled number is the black tile, the others are white
             for (var i = 0; i < 4; i++)
                 if (i != black) {
-                    this.addWhiteTile(0, i * 120);
+                    this.addWhiteTile(i * 120, -200);
                 } else {
-                    this.addBlackTile(0, i * 120);
+                    this.addBlackTile(i * 120, -200);
                 }
         }
 
