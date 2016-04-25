1ee2535a11bb36b2ca7a529f857273e564b70c9d
Linda <locoda@foxmail.com>
2016-04-24 22:02:33 -0400

    add setting into required ts

--- app.ts
+++ app.ts
@@ -23,7 +23,7 @@
 
 
 window.onload = () => {
-    requirejs([ 'game/boot','game/menu','game/game','game/main/normal','game/main/zen'], function () {
+    requirejs([ 'game/boot','game/menu','game/game','game/main/normal','game/main/zen', 'game/setting'], function () {
         var game = new BrickStep.Game;
     })
     
